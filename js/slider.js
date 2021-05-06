9ed7226b1e35c7fb57d4d320629783cd2ea0c93f
castelll0009 <castelll0009@gmail.com>
2021-05-06 18:59:43 -0500

    monton de errres

--- js/slider.js
+++ js/slider.js
@@ -1,5 +1,3 @@
-
-/*
 var swiper = new Swiper(".mySwiper", { 
 
         pagination: {
@@ -11,7 +9,8 @@ var swiper = new Swiper(".mySwiper", {
           prevEl: ".swiper-button-prev",
         },        
       });
-*/
+
+      /*variables */
 var tarjet_about = document.querySelector(".about");
 var tarjet_about_p = document.querySelector(".p-about");
 
@@ -20,15 +19,14 @@ var tarjet_contenido_p = document.querySelector(".p-contenido");
 
 var tarjet_orden  = document.querySelector(".orden");
 var tarjet_orden_h4 = document.querySelector(".h4-orden");
-/*
+
 var paginacion = document.querySelector(".swiper-pagination-progressbar-fill");
 
-paginacion_blanca();
+var entrar_detalles = document.querySelector(".entrar-detalles");
+var div_figure = document.querySelector(".div-figure");
+var contenedor_swiper_tarjetas = document.querySelector(".contenedor-swiper-tarjetas");
 
-function paginacion_blanca(argument) {    
-    paginacion.style = "background: white";
-}
-*/
+/*ADD EVENTS LISTENER ESCUCHADORES*/
 tarjet_about.addEventListener("click", function(){  
   tarjet_about.classList.toggle("desplegar-tarjeta-info");  
   tarjet_about_p.style ="opacity : 1; display : block;";  
@@ -41,8 +39,20 @@ tarjet_contenido.addEventListener("click", function(){
 
 tarjet_orden.addEventListener("click", function(){    
   tarjet_orden.classList.toggle("desplegar-tarjeta-orden");    
-/*  tarjet_orden_h4.style ="border-bottom: solid 1px black";  */
 });
+
+entrar_detalles.addEventListener("click", function(){    
+  div_figure.classList.toggle("desaparecer-contenedor"); 
+  contenedor_swiper_tarjetas.classList.toggle("aparecer-contenedor");
+});
+
+paginacion_blanca();
+
+/*funciiones */
+function paginacion_blanca(argument) {    
+    paginacion.style = "background: white";
+}
+
 
 
 
