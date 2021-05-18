b7e483982535d255af5db8b59dd44616df530227
castelll0009 <castelll0009@gmail.com>
2021-05-17 19:53:26 -0500

    opcion ordenar redirigir a  whatsapp

--- js/slider.js
+++ js/slider.js
@@ -29,7 +29,7 @@ tarjet_contenido.forEach( (contenido) => {
   contenido.addEventListener("click", function(){           
     contenido.classList.toggle("desplegar-tarjeta-info");  
     /*about_p.style ="opacity : 1; display : block;";  */
-    contenido.querySelector("p").style ="opacity : 1; display : block;";   // esta linea vale oro
+    contenido.querySelector("p").style ="opacity : 1; display : block;";   // esta linea vale oro    
   });
 });
 /*ORDEN*/
@@ -80,7 +80,6 @@ entrar_detalles.forEach( (detalle,index) => {
   });
 });
 
-
 //desplegar todas los menus 
 
 function desplegarItems(){     
@@ -92,6 +91,45 @@ function desplegarItems(){
       tarjeta.classList.toggle("aparecer-contenedor");           
   });
 }
+/* llenar formulario contenido */
+var select_tipo_ancheta ;
+var selected, descripcion;
+function accionSelect(){  
+  event.stopPropagation(); 
+}
+var input_descripcion;
+function accionDescripcion(){
+  event.stopPropagation();
+}
+/*funcion coiprar en la tarjeta de  Ordenar*/
+document.getElementById("btn-comprar").addEventListener("click", comprarProducto);
+
+var URL_orden = window.location;
+var producto = "ANCHETAS";
+function comprarProducto(){
+  
+  event.stopPropagation();
+  
+  if(confirm("¿Esta seguro de realizar esta compra?") ){
+    alert("Su pedido sera enviado al Whatsapp del vendedor");
+    //recuperacion de los datos del contenido, tipo de ancheta selector y input description
+    select_tipo_ancheta = document.getElementById("select-tipo-ancheta");
+    selected = select_tipo_ancheta.options[select_tipo_ancheta.selectedIndex].text;
+
+    input_descripcion = document.getElementById("input-descripcion");
+    descripcion = input_descripcion.value;
+    // implemetar enlace     
+    var cadenaURL = "https://wa.me/573202486769?text="+producto+":"+selected+"%20"+"DESCRIPCION: "+descripcion;
+    URL_orden =  window.location= cadenaURL;
+  }else{
+    
+  }
+}
+
+
+
+
+
 
 
 
