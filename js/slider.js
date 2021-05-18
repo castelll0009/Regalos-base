181ad09eda707d4d49656b4274af9483a6425594
castelll0009 <castelll0009@gmail.com>
2021-05-18 18:24:01 -0500

    numero kelly

--- js/slider.js
+++ js/slider.js
@@ -160,6 +160,7 @@ var descripcion_producto_vender;
 var TOTAL_PAGAR_producto_vender;
 //variables para crear URL y enviar pedido a Whatsapp
 /*var numero_telefono = 573202486769;*/
+/**/ 
 var numero_telefono = 573224338473;
 var URL_orden = window.location;
 var cadenaURL = "";
